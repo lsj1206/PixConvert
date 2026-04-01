09bd8e41f583d0f9614a841f1c3fb13f72ee7a3a
lsj1206 <1007ckddjs12@gmail.com>
2026-04-02 00:10:50 +0900

    Feat: Add Converting List

---
+++ src/Views/Lists/ConvertingListControl.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+
+namespace PixConvert.Views.Lists;
+
+/// <summary>
+/// 변환 중인 파일 현황을 표시하기 위한 전용 리스트 뷰입니다.
+/// </summary>
+public partial class ConvertingListControl : UserControl
+{
+    public ConvertingListControl()
+    {
+        InitializeComponent();
+    }
+}
